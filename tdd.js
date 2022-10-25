07b4bd905a1aeebbf7a8fc490e9a655d5e0577aa
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2022-10-25 14:10:14 +0200

    Add calculator object

--- tdd.js
+++ tdd.js
@@ -5,3 +5,21 @@ export function capitalize(str) {
 export function reverseString(str) {
   return str.split('').reverse().join('');
 }
+
+export const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  divide(a, b) {
+    if (b === 0) {
+      throw new Error('Cannot divide by zero!');
+    }
+    return a / b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+};
